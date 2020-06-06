f5d24fd050fc3277eb65944015d2a048370e1bfb
139pavlusha <pavel1grek@gmail.com>
2020-06-06 16:51:23 +0300

    Happy end

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+$(document).ready(function () {
+    $('.slides').slick({
+        arrows: true,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        pauseOnHover: false
+    });
+});
